0d91789da00481a0535ea9b071f3cfc832ebb7f2
Moonlight456 <2321755720@qq.com>
2019-06-18 15:01:49 +0800

    添加

--- todolist/views.py
+++ todolist/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+lst=[
+    {'待办事项':'吃饭','已完成':False},
+    {'待办事项':'逛街','已完成':False}
+]
 
 def home(request):
-    return render(request,'home.html')
+    global lst
+
+    print(request.method)
+    if request.method=="POST":
+        content=request.POST.get('待办事项')
+        print(content)
+        if not content and content.strip() == '':
+            return render(request, 'todolist/home.html', {'清单': lst,'警告':'请输入内容'})
+        else:
+            lst.append({'待办事项': content, '已完成': False })
+            return render(request,'todolist/home.html',{'清单':lst})
+    else:
+        return render(request, 'todolist/home.html', {'清单': lst})
+
 
 def edit(request):
-    return render(request,'edit.html')
+    return render(request,'todolist/edit.html')
 
 def about(request):
-    return render(request,'about.html')
+    return render(request,'todolist/about.html')
